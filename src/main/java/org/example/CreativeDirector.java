2181a948e846a092d61902475eddec2a37bace80
Mariya Byehan <maria_byehan@yahoo.com>
2025-05-06 19:46:11 +0200

    CreativeDirectorClass has been created

---
+++ src/main/java/org/example/CreativeDirector.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class CreativeDirector extends Employee {
+
+    public CreativeDirector(String name, byte age) {
+        super(name, age, "Creative Director");
+    }
+
+    public void createCampaign() {
+        System.out.println(getName() + " is creating a brilliant campaign.");
+    }
+}
+
